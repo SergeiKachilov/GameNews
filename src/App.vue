ccd5078c86adecc43eaf9df4a57decc2a1199704
SergeiKachilov <sergei.kachilov@yandex.ru>
2025-11-11 19:15:14 +0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+     import { ref } from "vue";
+     import mainPage from "./components/mainPage.vue";
+</script>
+
+<template>
+     <main-page></main-page>
+</template>
+
+<style scoped>
+
+</style>
